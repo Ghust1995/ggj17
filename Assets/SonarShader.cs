95ad9a8b7f82f32d2e98c0780841311d67f59e5c
Gustavo Guimaraes <Gustavo Guimaraes>
2017-01-21 16:20:35 -0800

    Added sonar shader

---
+++ Assets/SonarShader.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SonarShader : MonoBehaviour {
+
+    [Serializable]
+    public class SonarPoint
+    {
+        public Vector4 Position;
+        public float Radius;
+    }
+
+    public List<SonarPoint> Points;
+
+    public Material material;
+
+    public float WaveSpeed;
+
+    public Vector4 Position;
+    public float Speed;
+
+    public float MaxRadius;
+
+    private void Awake()
+    {
+        material.SetVectorArray("_Points", new Vector4[100]);
+        material.SetFloatArray("_Radius", new float[100]);
+    }
+
+    // Use this for initialization
+    void Start () {
+        if(Points.Count > 0)
+        {
+            material.SetInt("_Points_Length", Points.Count);
+            material.SetVectorArray("_Points", Points.Select((p) => p.Position).ToList());
+            material.SetFloatArray("_Radius", Points.Select((p) => p.Radius).ToList());
+        }        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Points = Points.Select((p) => {
+            p.Radius += Time.deltaTime * WaveSpeed;
+            return p;
+        }
+        ).ToList();            
+
+        var velocity = Speed * Time.deltaTime * new Vector4(Input.GetAxisRaw("Horizontal"), 0,  Input.GetAxisRaw("Vertical"), 0);
+        Position += velocity;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Points.Add(new SonarPoint()
+            {
+                Position = Position,
+                Radius = 0.2f,
+            });            
+        }
+
+        Points.RemoveAll((p) => p.Radius > MaxRadius);
+
+        material.SetInt("_Points_Length", Points.Count);
+        if(Points.Count > 0)
+        {
+            material.SetVectorArray("_Points", Points.Select((p) => p.Position).ToList());
+            material.SetFloatArray("_Radius", Points.Select((p) => p.Radius).ToList());
+        }        
+    }
+}
---
+++ Assets/Test.shader
@@ -0,0 +1,79 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Custom/Test"
+{
+	Properties{
+		_ColorMap("Height Map", 2D) = "white" {}
+		_Tex("Texture", 2D) = "white" {}
+		_Thickness("Thickness", Float) = 0.2
+		_Decay("Decay", Float) = 0.2
+
+	}
+	SubShader {
+		Tags {"Queue" = "Transparent"}
+		Blend SrcAlpha OneMinusSrcAlpha
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct vertInput {
+				float4 pos : POSITION;
+				float2 texcoord : TEXCOORD0;
+				float4 color : COLOR;
+			};
+			struct vertOutput {
+				float4 pos : POSITION;
+				float2 texcoord : TEXCOORD0;
+				fixed3 worldPos : TEXCOORD1;
+				float4 color : COLOR;
+			};
+
+			vertOutput vert(vertInput input) {
+				vertOutput o;
+				o.pos = mul(UNITY_MATRIX_MVP, input.pos);
+				o.worldPos = mul(unity_ObjectToWorld, input.pos).xyz;
+				o.texcoord = input.texcoord;
+				o.color = input.color;
+				return o;
+			}
+
+
+			uniform int _Points_Length;		
+
+			uniform float3 _Points[100];		
+			uniform float _Radius[100];
+
+			uniform float _Thickness;
+			uniform float _Decay;
+			uniform float _NormalParam;
+			uniform float _FadeTime;
+
+			sampler2D _Tex;
+			sampler2D _ColorMap;
+
+			half4 frag(vertOutput output) : COLOR {
+				half h = 0;			
+				for (int i = 0; i < _Points_Length; i++) {
+					half di = distance(output.worldPos, _Points[i].xyz);
+					
+					half ri = _Radius[i];
+					//if (di < ri - _Thickness || di > ri + _Thickness) continue;
+					half hi = saturate(1 - _Decay * abs(di - ri));
+					
+					h += hi;
+				}
+
+				h = saturate(h);
+				half4 color = half4(0.0, 0.0, 0.0, 1.0);
+				if (h > 0) {
+					color += h * tex2D(_Tex, output.texcoord) * tex2D(_ColorMap, output.texcoord);
+				}
+				return color;
+			}
+			ENDCG
+		}
+	}
+	Fallback "Diffuse"
+}
